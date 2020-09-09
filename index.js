a3abc23c7740d4ef0b1d3d653a2e3ecf2c672f01
emabytes <emadavor22@gmail.com>
2020-09-09 23:51:04 +0200

    first

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express")
+const app = express()
+const data = require("./data.json")
+const bodyParser = require('body-parser')
+
+//random 6 elements
+// const newArr = [];
+// for (let i = 0; i < 7; i++) {
+//     let random = Math.floor(Math.random() * data.length);
+//     newArr.push(data[random]);
+//     data.splice(random, 1);
+// }
+// console.log(newArr);
+
+//first 6 elements
+const newArr = data.slice(0, 7)
+console.log(newArr)
+
+app.set("view engine", "ejs")
+app.use(express.static("public"))
+var jsonParser = bodyParser.json()
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.listen(5050, () => {
+    console.log("server at http://localhost:5050")
+})
+
+app.get("/", (req, res) => {
+    res.render("index", { title: "Home", data: data })
+})
+
+app.get("/newArticle", (req, res) => {
+    res.render("newArticle", { title: "New Article", newArr: newArr })
+})
+
+app.post('/contactData', urlencodedParser, (req, res) => {
+    console.log(req.body)
+    console.log(req.body.name);
+    console.log(req.body.url);
+    console.log(req.body.author);
+    console.log(req.body.message);
+
+    res.status(201).send("created")
+})
+
+app.use((req, res) => {
+    res.render('404', { title: 'Not found' })
+})
